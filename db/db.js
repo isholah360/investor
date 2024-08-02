0988fac52ab09db03eebf046103dd5729562ef1e
ishola360 <isholah360.com>
2024-08-02 07:08:16 +0100

    databaefix

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const riskperDb = async () => {
     try {
-        const konect = await mongoose.connect("mongodb+srv://babalolah360:4ulKnzOtrfcl4es0@risk.pfyrgkn.mongodb.net/risk?retryWrites=true&w=majority&appName=risk")
+        const konect = await mongoose.connect(process.env.MONGO_URI)
         console.log(`risk app Server is connecetd to ${konect.connection.host}`)
         
     } catch (error) {
